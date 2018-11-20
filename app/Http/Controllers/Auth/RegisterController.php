a577002ee18f624d4e6a7354c3012922ea9468ca
Christian Aldover <cka6@njit.edu>
2018-11-20 13:33:29 -0800

    added fake question data to new users

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -62,9 +62,21 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+//        return User::create([
+//            'email' => $data['email'],
+//            'password' => Hash::make($data['password']),
+//        ]);
+
+        // Creates 50 fake questions after registering a new account
+        $user = User::create([
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        for ($i = 1; $i <= 50; $i++) {
+            $question = factory(\App\Question::class)->make();
+            $question->user()->associate($user);
+            $question->save();
+        }
+        return $user;
     }
 }
