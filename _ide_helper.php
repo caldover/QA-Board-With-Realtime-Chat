2d69e200001f05cbaa44f30af4ba5a9d6d70d69f
Christian Aldover <cka6@njit.edu>
2018-11-20 14:27:39 -0800

    updated composer.lock with composer update

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.13 on 2018-11-20 20:46:13.
+ * Generated for Laravel 5.7.13 on 2018-11-20 22:26:52.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
