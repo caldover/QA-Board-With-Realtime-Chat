29e37874bd92c6d3c575cfbd05663ca0068b76a1
Christian Aldover <cka6@njit.edu>
2018-11-20 12:22:15 -0800

    attempting to fix QueryException error

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-20 02:46:49.
+    * Generated on 2018-11-20 20:09:19.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.13 on 2018-11-20 02:46:49.
+ * Generated for Laravel 5.7.13 on 2018-11-20 20:09:18.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
